68e48010d8efbb94bba8acdb8c33cc23add5c64f
Hemashree0713 <hemas2026@gmail.com>
2025-03-24 14:14:19 +0530

    Add files via upload

---
+++ stringpalindromecheck.java
@@ -0,0 +1,13 @@
+class stringpalindromecheck
+{
+	public static void main(String[] args)
+	{
+		String str1="Malayalam";
+		StringBuffer s1=new StringBuffer(str1);
+		StringBuffer s2=s1.reverse();
+		if(str1.equalsIgnoreCase(s2.toString()))
+			System.out.println("Palindrome");
+		else
+			System.out.println("Not a Palindrome");
+	}
+}
