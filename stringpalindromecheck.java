d84a6c8a49f9839405c346e6855e84a9d4cfd0a0
Hemashree0713 <hemas2026@gmail.com>
2025-03-24 15:07:24 +0530

    Update stringpalindromecheck.java

--- stringpalindromecheck.java
+++ stringpalindromecheck.java
@@ -2,7 +2,7 @@ class stringpalindromecheck
 {
 	public static void main(String[] args)
 	{
-		String str1="Malayalam";
+		String str1="MOM";
 		StringBuffer s1=new StringBuffer(str1);
 		StringBuffer s2=s1.reverse();
 		if(str1.equalsIgnoreCase(s2.toString()))
